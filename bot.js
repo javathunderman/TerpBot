24bbc902e48dffe27cf2efc5118daaaaa7cf69c3
Kali Elaina <kali@new-host-20.home>
2020-09-04 15:59:02 -0400

    Added more commands, started working with roles.

--- bot.js
+++ bot.js
@@ -15,13 +15,23 @@ var bot = new Discord.Client({
 
 client.once('ready', () => {
     client.on('message', message => {
-        if (!message.content.startsWith(prefix) || message.author.bot) return;
+        const roleList = ['league', 'rocket', 'overwatch', 'gaming'];
+        const roleListID = ['751507896541380670', '751507831554965576', '751507860499857580', '751479288502681664']
 
         const args = message.content.slice(prefix.length).trim().split(/ +/);
         const command = args.shift().toLowerCase();
 
-        // commands
-        if (command === 'ping') {
+        if (!message.content.startsWith(prefix) && !message.author.bot) {
+            if (message.content.includes('^')) {
+                message.channel.send("^^^");
+            } else if ((message.content.includes("hw") || message.content.includes("homework"))) {
+                if (args.length !== 0) {
+                    message.channel.send("***DO YOUR HOMEWORK CHILD***");
+                } else {
+                    return;
+                }
+            }
+        } if (command === 'ping') {
             message.channel.send('pong!');
         } else if (command === 'links') {
             message.channel.send("Testudo: https://testudo.umd.edu/ \nELMS: https://elms.umd.edu/ \nCMSC131 Homepage: http://www.cs.umd.edu/class/fall2020/cmsc131-010X-030X/index.shtml ");
@@ -29,27 +39,31 @@ client.once('ready', () => {
             message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
         } else if (command === 'login') {
             message.channel.send("Username: cmsc131 \nPassword: sprcoredump");
-        } else if (command === 'nerd') {
-            
-        }
+        } else if (command === 'giveme' && args.length !== 0) {
+            var user = message.mentions.members.first();
+            for (var i = 0; i < roleList.length; i++) {
+                if (roleList[i] === args[1]) {
+                    user.roles.add(roleListID[i]);
+                    message.channel.send(`Gave ${user.displayName} the role.`);
+                    console.log(`Added ${roleList[i]} to ${user.displayName}.`)
+                    return;
+                }
+            }
 
-        // contains text 
-        else if (message.content === '^') {
-            message.channel.send('^^^');
-        } else if (message.content.includes('rocky') || message.content.includes('Rocky')) {
-            message.channel.send('Which one?');
-        } else if (message.content.includes('is a nerd')) {
-            message.channel.send("no u");
-        } else if (message.content.includes('heck')) {
-            message.channel.send("hecc");
-        } else if (message.content.includes('terpbot is dumb')) {
-            message.channel.send('NO U');
-        } else if (message.content.includes('terpbot is not dumb')) {
-            message.channel.send('damn straight');
-        } else if (message.content.includes('trans')) {
-            message.channel.send('Trans rights!');
-        } else if (message.content.includes('shut up')) {
-            message.channel.send("make pizza not war children");
+        } else if (command === 'takerole' && args.length !== 0) {
+            var u = message.mentions.members.first();
+            var foundRole = false;
+            for (var i = 0; i < roleList.length; i++) {
+                if (roleList[i] === args[1]) {
+                    foundRole = true;
+                    u.roles.remove(roleListID[i]);
+                    console.log(`Removed ${roleList[i]} from ${u.displayName}.`)
+                    message.channel.send(`Removed ${u.displayName}'s role.`);
+                    return;
+                }
+            }
+            message.channel.send(`${u.displayName} does not have this role. `);
+            return;
         }
 
     });
