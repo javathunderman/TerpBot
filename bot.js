954640f1aa2f8cd2c51f9c35ccbc190a192ff952
Kali Elaina <kali@new-host-4.home>
2020-09-07 14:21:34 -0400

    worked more on giveme command

--- bot.js
+++ bot.js
@@ -15,8 +15,8 @@ var bot = new Discord.Client({
 
 client.once('ready', () => {
     client.on('message', message => {
-        const roleList = ['league', 'rocket', 'overwatch', 'gaming'];
-        const roleListID = ['751507896541380670', '751507831554965576', '751507860499857580', '751479288502681664']
+        const roleList = ['league', 'rocket', 'overwatch', 'gaming', 'developer', 'cs-meetup'];
+        const roleListID = ['751507896541380670', '751507831554965576', '751507860499857580', '751479288502681664', '752585354560798800', '752585329789108334']
 
         const args = message.content.slice(prefix.length).trim().split(/ +/);
         const command = args.shift().toLowerCase();
@@ -40,14 +40,27 @@ client.once('ready', () => {
         } else if (command === 'login') {
             message.channel.send("Username: cmsc131 \nPassword: sprcoredump");
         } else if (command === 'giveme' && args.length !== 0) {
-            var user = message.mentions.members.first();
-            for (var i = 0; i < roleList.length; i++) {
-                if (roleList[i] === args[1]) {
-                    user.roles.add(roleListID[i]);
-                    message.channel.send(`Gave ${user.displayName} the role.`);
-                    console.log(`Added ${roleList[i]} to ${user.displayName}.`)
-                    return;
+            try {
+                var isFound = false;
+                var user = message.mentions.members.first();
+                for (var i = 0; i < roleList.length; i++) {
+                    if ((roleList[i] === args[1])) {
+                        isFound = true;
+                        if (message.member.roles.cache.has(roleListID[i]) === false) {
+                            user.roles.add(roleListID[i]);
+                            console.log(`Added ${roleList[i]} to ${user.displayName}.`);
+                            message.channel.send(`Gave ${user.displayName} the role.`);
+                            return;
+                        } else if (message.member.roles.cache.has(roleListID[i]) === true) {
+                            message.channel.send("User already has this role!");
+                            return;
+                        }
+                    }
+                } if (!isFound) {
+                    message.channel.send("Could not find the specified role.");
                 }
+            } catch (err) {
+                return;
             }
 
         } else if (command === 'takerole' && args.length !== 0) {
